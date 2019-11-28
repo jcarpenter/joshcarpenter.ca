d74a4bec5a886f817dd5e4eb501e52d04df1ad40
Josh <rocarpenca@gmail.com>
2019-11-27 16:53:43 -0800

    Updated icon css, again x 36

--- src/js/focus-test.js
+++ src/js/focus-test.js
@@ -23,7 +23,7 @@
     // }
 
     function setupLinks() {
-        
+
         //var test = document.querySelector('#testItem')
         //test.addEventListener("touchend", touchEnd, false); // Works
         //test.addEventListener("click", itemClick, false) // Works
@@ -31,7 +31,30 @@
         //test.addEventListener("touchcancel", touchCancel, false);
 
         var article = document.querySelector('article')
-        article.addEventListener('touchend', touchEnd, false)
+        var selected
+        
+        if (window.PointerEvent) {
+
+            article.addEventListener('pointerdown', (e) => {
+                if (e.pointerType == 'touch') {
+                    
+                    // console.log("article: touch detected")
+                    
+                    if (selected) {
+                        // console.log("selected blurred")
+                        selected.blur()
+                        selected = null
+                    }
+
+                    if (e.target.tagName == "BUTTON") {
+                        // console.log("button tapped")
+                        selected = e.target
+                    }
+                }
+            });
+        }
+
+        // article.addEventListener('touchend', touchEnd, false)
 
     }
 
@@ -42,7 +65,7 @@
     // Listen for taps on article.
     // Check if element tapped had tabindex value
     // If yes, save element to variable.
-    
+
     // Listen for subequent taps on article.
     // Check if element tapped was the saved element.
     // If no, blur the saved element.
